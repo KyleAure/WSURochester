827b338b57fa4508d6728db4cca30ac5a17a586d
Kyle Aure <kaure09@winona.edu>
2018-10-01 11:30:26 -0500

    Intermediate commit on project 2

---
+++ CS415/Project2/src/Project2.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Project2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void Log(const char* message){
+	std::cout << message << std::endl;
+}
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
