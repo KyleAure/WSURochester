a361c25612e8d4f73884e4da359a49707d90fa27
Kyle Aure <kaure09@winona.edu>
2018-11-26 12:28:00 -0600

    Fixed project 3

--- CS415/Project3/src/test1.cpp
+++ CS415/Project3/src/test1.cpp
@@ -6,28 +6,28 @@ using namespace std;
 
 void Error(const char *text)
 {
-   std::cout << "ERROR: " << text << std::endl;
-   exit(-1);
+    cout << "ERROR: " << text << endl;
+    exit(-1);
 }
 
 int main(int argc, char **argv)
 {
-   Ptr<int> foo(new int(12));
-   Ptr<int> tmp((int*)NULL);
-   Ptr<int> bar = tmp;
+    Ptr<int> foo(new int(12));
+    Ptr<int> tmp((int*)NULL);
+    Ptr<int> bar = tmp;
 
-   if (foo == 0)
-   Error("Foo shouldn't be null!");
-   if (bar != 0)
-   Error("Bar should be null!");
-   bar = new int(12);
-   if (foo == bar)
-   Error("Foo and bar are distinct pointers!");
-   if (*foo != *bar)
-   Error("Foo and bar should have the same value here!");
+    if (foo == 0)
+	Error("Foo shouldn't be null!");
+    if (bar != 0)
+	Error("Bar should be null!");
+    bar = new int(12);
+    if (foo == bar)
+	Error("Foo and bar are distinct pointers!");
+    if (*foo != *bar)
+	Error("Foo and bar should have the same value here!");
 
-   release(foo);
-   release(bar);
-   cout << "foo1: OK" << endl;
-   return 0;
+    release(foo);
+    release(bar);
+    cout << "foo1: OK" << endl;
+    return 0;
 }
