b44bf02d64d27163e693e5e44f6da4a36dc822d9
Kyle Aure <kaure09@winona.edu>
2019-03-27 16:54:58 -0500

    Had to change class based on dicussion in class.

--- CS410/Assignment8/src/edu/winona/cs/CS410/Sum.java
+++ CS410/Assignment8/src/edu/winona/cs/CS410/Sum.java
@@ -5,47 +5,35 @@ public class Sum implements Summing {
 	@Override
 	public int sum(int[] input, PossiblyThrow thrower) {
 		//Assume input array is empty
-		int result = 0; 
+		long result = 0;
+		int intResult = 0;
 		
-		//If null pointer return Integer.MIN_VALUE
 		if(input == null) {
-			result = Integer.MIN_VALUE;
-		} else if(input.length != 0) {
+			intResult = Integer.MIN_VALUE;
+		} else if(input.length == 0) {
+			intResult = 0;
+		} else if(input.length != 0){
 			for(int i = 0; i < input.length; i++) {
 				try {
-					thrower.check(input[i]); //Throws exception before overflow/underflow occures.
+					thrower.check(input[i]);
 					result += input[i];
-				//Returns Integer.MAX_VALUE if the sum is greater or equal to Integer.MAX_VALUE.
-				} catch (OverflowException oe) {
-					result = Integer.MAX_VALUE;
-					break;
-				//Returns Integer.MIN_VALUE if the sum is lesser or equal to Integer.MIN_VALUE.  
-				} catch (UnderflowException ue) {
-					result = Integer.MIN_VALUE;
-				//For all error conditions, (i.e. null pointer exception), Integer.MIN_VALUE should be returned.
+					System.out.println("input=" + input[i] + " result=" + result);
 				} catch (Exception e) {
-					result = Integer.MIN_VALUE;
+					intResult = Integer.MIN_VALUE;
 					break;
 				}
+			}//end sum
+			
+			if(result >= Integer.MAX_VALUE) {
+				intResult = Integer.MAX_VALUE;
+			} else if(result <= Integer.MIN_VALUE) {
+				intResult = Integer.MIN_VALUE;
+			} else {
+				intResult = (int) result;
 			}
 		}
+		
 		//Return calculated result
-		return result;
-	}
-
-	//Custom OverflowException
-	class OverflowException extends Exception {
-		private static final long serialVersionUID = 1L;
-		public OverflowException(String errorMessage) {
-			super(errorMessage);
-		}
-	}
-	
-	//Custom UnderflowException
-	class UnderflowException extends Exception {
-		private static final long serialVersionUID = 1L;
-		public UnderflowException(String errorMessage) {
-			super(errorMessage);
-		}
+		return intResult;
 	}
 }
