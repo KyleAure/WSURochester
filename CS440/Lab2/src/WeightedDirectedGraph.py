f8df927cb961c7db1ed7f798d979b34ec22a4d70
KyleAure <kylejaure@gmail.com>
2019-07-31 23:36:22 -0500

    finished lab 2

--- CS440/Lab2/src/WeightedDirectedGraph.py
+++ CS440/Lab2/src/WeightedDirectedGraph.py
@@ -2,36 +2,113 @@
 
 # Name: Kyle Aure
 # Course: CS440
-# Date: 07/24/2019 
+# Date: 07/31/2019 
 #####  DESCRIPTION  #####
 # This class represents a directed weighted 
 # graph it uses a 2-D array as the underlying 
-# data structre to keep track of nodes, edges, and weights
-from collections import defaultdict
-import graphs
-import json
+# data structure to keep track of nodes, edges, and weights
+import sys
 
-#tag::class[]
+# tag::class[]
 class WeightedDirectedGraph:
     # Constructor
     def __init__(self, nodes):
-        self.m = nodes # Initialize m to number of nodes          
+        self.n = nodes # Initialize n to number of nodes          
         
-        # Initialize 2-D array of size m*m
-        # If weights[u][v] > 0 then 
+        # Initialize 2-D array of size n*n
+        # If weights[u][v] > -1 then 
         # there exits an edge (u,v) 
         # and it's weight is val(weights[u][v])
-        self.weights = [[0 for columns in range(nodes)] for rows in range(nodes)]
+        self.weights = [[-1 for columns in range(nodes)] for rows in range(nodes)]
   
     # Function to add an edge
     def addEdge(self, parent, child, weight):
         self.weights[parent][child] = weight
 
-    def minDistance(self, )
+    # tag::helper[]
+    # A helper function to find the next node,
+    # which has the shortest distance,
+    # but has not yet been put on the shortest path list.
+    def minDistance(self, dist, onPath):
+        # Init min to infinity
+        min = sys.maxsize
 
-#end::class[]
+        # Search for the nearest node not
+        # already in the shortest path list.
+        for node in range(self.n):
+            if dist[node] < min and onPath[node] == False:
+                min = dist[node]
+                index = node
+
+        return index
+    # end::helper[]
+    # tag::dijkstra[]
+    def dijkstra(self, source):
+        # Variable needed for dijkstra
+        dist = [sys.maxsize] * self.n       # dist[x] is the distance from source to x
+        dist[source] = 0                    # Initialize distance from source to source = 0
+        onPath = [False] * self.n           # onPath[x] is true if x has been found to be on the shortest path
+        path = [[] for i in range(self.n)]  # path[x] will give path from source to x in order
+        path[source].append(source)         # Initialize the path from source to source
+
+        for node in range(self.n):
+            # Use our helper function to find next node
+            # to be processed.  That is the node with the least distance
+            # that is not already on the shortest distance path.
+            u = self.minDistance(dist, onPath)
+
+            # Put this on the shortest path 
+            onPath[u] = True
+
+            # Go through each node
+            for v in range(self.n):
+                # If we have found a connecting node
+                # and it is not yet on the shortest path
+                # and the current distance of v is more than the distance of u plus this path
+                # Then we have found a shorter path
+                if (self.weights[u][v] > -1) and (onPath[v] == False) and (dist[v] > dist[u] + self.weights[u][v]):
+                    # Update the distance of the shortest path
+                    dist[v] = dist[u] + self.weights[u][v]
+                    # Update the actual path array
+                    path[v] = path[u] + [v]
+        
+        return dist, path
+    # end::dijkstra[]
+# end::class[]
+# tag::main[]
+# Helper function to print results
+def printResults(graph, dist, path):
+    print("\n----Graph----\n")
+    for row in range(graph.n):
+        print("Node: {}\tMap: {}".format(row,graph.weights[row]))
+
+    print("\n----Shortest Path----\n")
+    for row in range(graph.n):
+        print("Node: {}\tPath: {}".format(row, path[row]))
+
+    print("\n----Minimum Distance----\n")
+    for row in range(graph.n):
+        print("Node: {}\tDistance: {}".format(row, dist[row]))
+
 # main runs and prints example graphs
 def main():
-    pass
+    size = 9
+    source = 0
+    graph = WeightedDirectedGraph(size) 
+    graph.weights = [
+        [-1, 4, -1, -1, -1, -1, -1, 8, -1], 
+        [4, -1, 8, -1, -1, -1, -1, 11, -1], 
+        [-1, 8, -1, 7, -1, 4, -1, -1, 2], 
+        [-1, -1, 7, -1, 9, 14, -1, -1, -1], 
+        [-1, -1, -1, 9, -1, 1-1, -1, -1, -1], 
+        [-1, -1, 4, 14, 1-1, -1, 2, -1, -1], 
+        [-1, -1, -1, -1, -1, 2, -1, 1, 6], 
+        [8, 11, -1, -1, -1, -1, 1, -1, 7], 
+        [-1, -1, 2, -1, -1, -1, 6, 7, -1] 
+        ]
+    
+    dist, path = graph.dijkstra(source) 
+    printResults(graph, dist, path)
+# end::main[]
 if __name__ == '__main__':
     main()
--- CS440/Lab2/src/test.py
+++ CS440/Lab2/src/test.py
@@ -2,53 +2,43 @@
 
 # Name: Kyle Aure
 # Course: CS440
-# Date: 07/23/2019 
+# Date: 07/31/2019 
 #####  DESCRIPTION  #####
-# This class tests our directed graph class and functions
+# This class tests our weighted directed graph class and functions
 
 import unittest
-from graphs import *
-from DirectedGraph import DirectedGraph
-
-class testDirectedGraph (unittest.TestCase):
+from WeightedDirectedGraph import WeightedDirectedGraph, printResults
+#tag::test[]
+class testWeightedDirectedGraph (unittest.TestCase):
     def testAlwaysTrue(self):
         self.assertTrue(True, "This should always be true")
 
-    def testEmptyGraph(self):
-        result, cycle = emptyGraph.evaluateAsDAG()
+    def testDistances(self):
+        graph = WeightedDirectedGraph(2)
+        graph.addEdge(0,1,5)
+        self.assertEqual(5, graph.weights[0][1], "Distance from 0 to 1 should be 5")
+        self.assertEqual(-1, graph.weights[1][0], "Distance from 1 to 0 should be -1")
 
-        self.assertIsNone(result, "Empty graph should not be a DAG")
-        self.assertIsNone(cycle, "Empty graph should not have initialized a cycle")
-        self.assertIsNone(emptyGraph.isDAG(), "Empty graph should not be a DAG")
-        with self.assertRaises(Exception):
-            emptyGraph.getTopologicalOrdering()
-
-    def testCyclicGraph(self):
-        with self.assertRaises(Exception):
-            cyclicGraph.getTopologicalOrdering()
-        result, cycle = cyclicGraph.evaluateAsDAG()
-        expectedCycle = [2,3,1,0]
-
-        self.assertFalse(result, "Cyclic graph should not be a DAG")
-        self.assertIsNotNone(cycle, "Cyclic graph should have cycle")
-        self.assertEqual(expectedCycle, cycle, "Cyclic graph did not return correct cycle")
-        self.assertFalse(cyclicGraph.isDAG(), "Cyclic graph should not be a DAG")
-        with self.assertRaises(Exception):
-            cyclicGraph.getTopologicalOrdering()
-
-    def testNonCyclicGraph(self):
-        with self.assertRaises(Exception):
-            nonCyclicGraph.getTopologicalOrdering()
-        result, cycle = nonCyclicGraph.evaluateAsDAG()
-        expectedCycle = []
-        expectedTopOrder = [8, 7, 2, 3, 0, 1, 6, 9, 11, 12, 10, 5, 4]
-
-        self.assertTrue(result, "NonCyclic graph should be a DAG")
-        self.assertIsNotNone(cycle, "NonCyclic graph should have empty cycle")
-        self.assertEqual(expectedCycle, cycle, "NonCyclic graph did not return empty cycle")
-        self.assertTrue(nonCyclicGraph.isDAG(), "NonCyclic graph should be a DAG")
-        self.assertEqual(expectedTopOrder, nonCyclicGraph.getTopologicalOrdering(), "NonCyclic graph did not return expected topological ordering.")
+    def testDijkstra(self):
+        graph = WeightedDirectedGraph(5)
+        graph.weights = [
+            [-1,3,-1,4,-1],     #0 --(3)--> 1, 0 --(4)--> 3
+            [-1,-1,3,-1,-1],    #1 --(3)--> 2
+            [-1,-1,-1,-1,3],    #2 --(3)--> 4
+            [-1,-1,-1,-1,4],    #3 --(4)--> 4
+            [-1,-1,-1,-1,-1]    #Path [0,1,2,4] = 9, [0,3,4] = 8  
+        ]
+        dist, path = graph.dijkstra(0)
         
+        distance = graph.weights[0][1] + graph.weights[1][2] + graph.weights[2][4]
+        self.assertEqual(9, distance, "Path [0,1,2,4] should have a distance of 9")
+
+        distance = graph.weights[0][3] + graph.weights[3][4]
+        self.assertEqual(8, distance, "Path [0,3,4] should have a distance of 8")
+
+        self.assertEqual(8, dist[4], "Shortest path from 0 to 4 should be of distance 8")
+        self.assertEqual([0, 3, 4], path[4], "Shortest path from 0 to 4 should be [0, 3, 4]")
+#end::test[]
 # Unit test runner
 if __name__ == '__main__':
     unittest.main(verbosity=2)
